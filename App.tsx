108126e3c186d25b23923a5ad490f170fbc25b10
josephmagara <joseph.magara16@gmail.com>
2020-10-31 12:15:13 +1100

    Switched to using functional components.

--- App.tsx
+++ App.tsx
@@ -1,8 +1,9 @@
+import * as React from "react";
 import { StyleSheet, Text, View } from 'react-native';
 import AppRouter from './src/data/routing/AppRouter';
 
 export default function App() {
-  return (AppRouter());
+  return <AppRouter/>;
 }
 
 const styles = StyleSheet.create({
