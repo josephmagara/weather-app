05597517a15a6b4eb416197bfd01da0f631bd3bb
josephmagara <joseph.magara16@gmail.com>
2020-11-01 19:57:29 +1100

    Adding tests (enzyme and jest) to the app.

---
+++ __tests__/App.tsx
@@ -0,0 +1,24 @@
+import 'react-native';
+import App from '../App';
+import { shallow, ShallowWrapper } from 'enzyme';
+import React from 'react';
+import { View } from 'react-native';
+
+const createTestProps = (props: Object) => ({
+  ...props
+});
+
+describe("App", () => {
+  describe("rendering", () => {
+    let wrapper: ShallowWrapper;
+    let props: Object;
+    beforeEach(() => {
+      props = createTestProps({});
+      wrapper = shallow(<App />);
+    });
+  
+    it("should render a <View />", () => {
+      expect(wrapper.find(View)).toHaveLength(1);
+    });
+  });
+});
