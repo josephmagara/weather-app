b3e43bf73e1f7739a52911827c29a4961bc29ace
josephmagara <joseph.magara16@gmail.com>
2020-11-03 08:27:39 +1100

    Trying to fix the issue thats causing the app to fail to build

--- src/data/networking/WeatherGateway.tsx
+++ src/data/networking/WeatherGateway.tsx
@@ -1,15 +1,12 @@
-import { WEATHER_API_URL, ACCESS_KEY } from "react-native-dotenv";
 import { WeatherReportResponse } from "./WeatherReportResponse";
-import Toast from "react-native-simple-toast";
 import axios from "axios";
 
-const baseUrl = WEATHER_API_URL + "current?access_key=" + ACCESS_KEY;
+const baseUrl = process.env.WEATHER_API_URL + "current?access_key=" + process.env.ACCESS_KEY;
 
 export function getWeatherForCity(
   city: string
 ): Promise<WeatherReportResponse> {
   const queryString = baseUrl + "&query=" + city;
-  Toast.show(queryString, Toast.LONG);
   return new Promise<WeatherReportResponse>((resolve, reject) => {
     axios
       .get<WeatherReportResponse>(queryString)
